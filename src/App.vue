9f664b95fd715360012b187f8a4e8a32e474e6ca
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-06 12:58:25 +0200

    State example

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <Container title="Counter 1">
-    <h2>{{ counter }}</h2>
-    <button @click="counter++">+1</button>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="increment">+1</button>
+
+    <Counter />
   </Container>
 </template>
 
 <script>
 import Container from "./components/Container";
+import Counter from "./components/Counter";
 
 export default {
-  data() {
-    return {
-      counter: 0,
-    }
-  },
   components: {
     Container,
+    Counter,
   },
+  mounted() {
+    console.log(this);
+  },
+  methods: {
+    increment() {
+      this.$store.state.counter++;
+    }
+  }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.state.counter++;
+    },
+    decrement() {
+      this.$store.state.counter--;
+    },
+  }
+}
+</script>
