eeb5402970dd999b0f3dfdb63803d24b7407a765
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-06 12:58:16 +0200

    Vuex connected

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+
 import App from './App.vue'
+
+Vue.use(Vuex);
+const store = new Vuex.Store({
+  state: {
+    counter: 10,
+  },
+});
 
 Vue.config.productionTip = false
 
 new Vue({
   render: function (h) { return h(App) },
+  store: store
 }).$mount('#app')
