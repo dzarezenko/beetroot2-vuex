987f3c464d198d2a7b8c29c3f3a9c8407f7202e0
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-06 12:30:18 +0200

    Preparations

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-
+  <Container title="Counter 1">
+    <h2>0</h2>
+    <button>+1</button>
+  </Container>
 </template>
+
+<script>
+import Container from "./components/Container";
+
+export default {
+  components: {
+    Container,
+  },
+}
+</script>
---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+}
+</script>
+
+<style scoped>
+div {
+  display: block;
+  width: 400px;
+  margin: 0 auto;
+  -webkit-box-shadow: 5px 5px 15px 5px rgba(0,0,0,0.97); 
+  box-shadow: 5px 5px 15px 5px rgba(0,0,0,0.97);
+  padding: 30px;
+}
+</style>
